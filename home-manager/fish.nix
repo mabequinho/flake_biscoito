22d3c2fd11b90bd7116740a8fbe133b09d672043
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-14 14:52:56 -0300

    formatted fish.nix

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -1,21 +1,16 @@
 { config, ... }: {
-    programs= { 
-    fish = {
-        enable = true;
-        };
+  programs = {
+    fish = { enable = true; };
     mcfly = {
-        enable = true;
-        enableFishIntegration = true;
-        keyScheme = "vim";
-        enableLightTheme = true;
+      enable = true;
+      enableFishIntegration = true;
+      keyScheme = "vim";
+      enableLightTheme = true;
 
     };
-
     zoxide = {
-        enable = true;
-        enableFishIntegration = true;
+      enable = true;
+      enableFishIntegration = true;
     };
-    
-    };
-
+  };
 }
