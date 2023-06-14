704dfd8b10c273db795606e2c79fcb78353d0556
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-14 07:47:19 -0300

    basic home config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -35,8 +35,8 @@
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "mabeco";
+    homeDirectory = "/home/mabeco";
   };
 
   # Add stuff for your user as you see fit:
