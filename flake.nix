9fcfcdc1a956454955de54775ad55f083b1477a7
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-14 09:32:35 -0300

    removed quotes from home configuration

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "mabeco@biscoito" = home-manager.lib.homeManagerConfiguration {
+      mabeco@biscoito = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main home-manager configuration file <
