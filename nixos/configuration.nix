006c7db3cde0a6a2de2fa5515dfd12572db0cc8f
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-14 19:44:07 -0300

    added locales

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,6 +57,20 @@
   };
   environment.systemPackages = [ pkgs.git ];
   time.timeZone = "America/Fortaleza";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+  };
   security.sudo.wheelNeedsPassword = false;
   users.users = {
     mabeco = {
