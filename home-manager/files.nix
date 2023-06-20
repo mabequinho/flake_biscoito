35c04d128632be1229dba05ab25f1d7c8590d5a5
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-20 13:05:12 -0300

    editor settings

--- home-manager/files.nix
+++ home-manager/files.nix
@@ -36,15 +36,14 @@
     #};
   };
 
-  systemd.user.tmpfiles.rules = [
-
-    "D /tmp/mabeco-cache 0700 mabeco wheel 1w"
-    "L+ %h/.cache - - - - /tmp/mabeco-cache"
-
-    "D /tmp/mabeco-down 0700 mabeco wheel 1w"
-    "L+ %h/Downloads - - - - /tmp/mabeco-down"
-
-    "D /tmp/mabeco-scsh 0700 mabeco wheel 1w"
-    "L+ %h/Pictures/Screenshots - - - - /tmp/mabeco-scsh"
-  ];
+#  systemd.user.tmpfiles.rules = [
+#   "D /tmp/mabeco-cache 0700 mabeco wheel 1w"
+#    "L+ %h/.cache - - - - /tmp/mabeco-cache"
+#
+#    "D /tmp/mabeco-down 0700 mabeco wheel 1w"
+#    "L+ %h/Downloads - - - - /tmp/mabeco-down"
+#
+#    "D /tmp/mabeco-scsh 0700 mabeco wheel 1w"
+#    "L+ %h/Pictures/Screenshots - - - - /tmp/mabeco-scsh"
+#  ];
 }
---
+++ home-manager/programs/codium.nix
@@ -0,0 +1,30 @@
+{ config, ... }: {
+  vscode = {
+    enable = true;
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      asvetliakov.vscode-neovim
+      ms-python.python
+      ms-toolsai.jupyter
+      jnoortheen.nix-ide
+    ];
+    userSettings = {
+      "editor.fontFamily" = "Mononoki Nerd Font Mono";
+      "editor.lineHeight" = 24;
+      "editor.fontLigatures" = true;
+      "explorer.decorations.badges" = false;
+      "workbench.colorTheme" = "Quiet Light";
+      "window.menuBarVisibility" = "toggle";
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "nix.serverSettings" = {
+        nil = {
+          diagnostics = { ignored = [ "unused_binding" "unused_with" ]; };
+          formatting = { command = [ "nixfmt" ]; };
+        };
+      };
+    };
+  };
+}
