c987f42cb69db8923941abcd81f6ca23ca94383a
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-20 15:17:03 -0300

    added poweroff timers

--- nixos/systemd.nix
+++ nixos/systemd.nix
@@ -4,15 +4,27 @@
     timers = {
       poffm = {
         description = "Poweroff during morning";
-        timerConfig = { OnCalendar = "*-*-* 09:00:00"; };
+        timerConfig = {
+          OnCalendar = "*-*-* 09:00:00";
+          Unit = "systemd-poweroff.service";
+        };
+        wantedBy = [ "multi-user.target" ];
       };
       poffn = {
         description = "Poweroff during noon";
-        timerConfig = { OnCalendar = "*-*-* 12:00:00"; };
+        timerConfig = {
+          OnCalendar = "*-*-* 12:00:00";
+          Unit = "systemd-poweroff.service";
+        };
+        wantedBy = [ "multi-user.target" ];
       };
       poffe = {
         description = "Poweroff during evening";
-        timerConfig = { OnCalendar = "*-*-* 18:00:00"; };
+        timerConfig = {
+          OnCalendar = "*-*-* 18:00:00";
+          Unit = "systemd-poweroff.service";
+        };
+        wantedBy = [ "multi-user.target" ];
       };
     };
   };
