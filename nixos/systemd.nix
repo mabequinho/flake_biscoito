bff9d4ebf2d1c3eaea1bdbbffdc12cb82746f797
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-21 07:08:04 -0300

    automatic poweroff tweaks

--- nixos/systemd.nix
+++ nixos/systemd.nix
@@ -5,29 +5,27 @@
       poffm = {
         description = "Poweroff during morning";
         timerConfig = {
-          OnCalendar = "*-*-* 09..09:30";
+          OnCalendar = "*-*-* 09..09:30/3";
           Unit = "systemd-poweroff.service";
         };
         wantedBy = [ "multi-user.target" ];
-        onFailure = [ "systemd-poweroff.service" ];
       };
       poffn = {
         description = "Poweroff during noon";
         timerConfig = {
-          OnCalendar = "*-*-* 12..13:00";
+          OnCalendar = "*-*-* 12..13:00/3";
           Unit = "systemd-poweroff.service";
         };
         wantedBy = [ "multi-user.target" ];
-        onFailure = [ "systemd-poweroff.service" ];
       };
       poffe = {
         description = "Poweroff during evening";
         timerConfig = {
-          OnCalendar = "*-*-* 18..23:00";
+          OnCalendar = "*-*-* 18..23:59/3";
+          OnCalendar = "*-*-* 00..07:00/3";
           Unit = "systemd-poweroff.service";
         };
         wantedBy = [ "multi-user.target" ];
-        onFailure = [ "systemd-poweroff.service" ];
       };
     };
   };
