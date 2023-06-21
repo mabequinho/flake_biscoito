1954d68b9e60e400738cd04826c641efb5f52c30
Lucio <lucio_a.khfzo@aleeas.com>
2023-06-21 17:23:21 -0300

    Simplified brek units. thnx @TLATER

--- nixos/systemd.nix
+++ nixos/systemd.nix
@@ -2,47 +2,16 @@
 
   systemd = {
     timers = {
-      break-morning = {
-        description = "morning break";
+      break = {
+        description = "breaks";
         timerConfig = {
-          OnCalendar = "*-*-* 09:*:00/5";
-          Persistent = false;
-          Unit = "systemd-suspend.service";
-        };
-        wantedBy = [ "multi-user.target" ];
-      };
-      break-noon = {
-        description = "noon break";
-        timerConfig = {
-          OnCalendar = "*-*-* 12:*:00/5";
-          Persistent = false;
-          Unit = "systemd-suspend.service";
-        };
-        wantedBy = [ "multi-user.target" ];
-      };
-      break-afternoon = {
-        description = "afternoon break";
-        timerConfig = {
-          OnCalendar = "*-*-* 15:*:00/5";
-          Persistent = false;
-          Unit = "systemd-suspend.service";
-        };
-        wantedBy = [ "multi-user.target" ];
-      };
-      ### currently unable to set multiple OnCalendar lines.
-      break-evening = {
-        description = "evening break";
-        timerConfig = {
-          OnCalendar = "*-*-* 18..23:*:00/5";
-          Persistent = false;
-          Unit = "systemd-suspend.service";
-        };
-        wantedBy = [ "multi-user.target" ];
-      };
-      break-dawn = {
-        description = "dawn break";
-        timerConfig = {
-          OnCalendar = "*-*-* 00..07:*:00/5";
+          OnCalendar = [
+          "*-*-* 09:*:00/5"
+          "*-*-* 12:*:00/5"
+          "*-*-* 15:*:00/5"
+          "*-*-* 18..23:*:00/5" 
+          "*-*-* 00..07:*:00/5"
+          ];
           Persistent = false;
           Unit = "systemd-suspend.service";
         };
