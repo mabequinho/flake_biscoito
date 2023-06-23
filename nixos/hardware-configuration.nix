c7cbd24426ec63dfee8292a846beb1669399b242
Lucio <46428150+mabequinho@users.noreply.github.com>
2023-06-23 12:30:47 +0000

    removed no longer used hardware.video.hidpi

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -89,6 +89,4 @@ keyFile = "/keyfile_biscoito.bin";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }
