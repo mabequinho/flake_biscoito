cea8716c46e861ce4b17c897248d62840e048cac
Lucio <lucio_a.khfzo@aleeas.com>
2023-07-04 11:55:56 -0300

    stuff

--- home/hypr/waybar.nix
+++ home/hypr/waybar.nix
@@ -1,4 +1,7 @@
 { config, pkgs, libs, inputs, ... }: {
+  # fix for  https://github.com/nix-community/home-manager/issues/4099
+  systemd.user.services.waybar.Service.Environment =
+    "PATH=${config.home.profileDirectory}/bin";
   programs = {
     waybar = {
       enable = true;
@@ -11,10 +14,10 @@
         microbar = {
           layer = "top";
           position = "bottom";
-#          width = 600;
+          #          width = 600;
           spacing = 6;
-          modules-left = [ "wlr/workspaces" ];
-          modules-center = [ ];
+          modules-left = [ "wlr/workspaces" "wireplumber" ];
+          modules-center = [ "wlr/taskbar" ];
           modules-right = [ "tray" "idle_inhibitor" "clock" ];
           "wlr/workspaces" = {
             "format" = ''<span size="large">{icon}</span>'';
@@ -35,19 +38,30 @@
           };
           "clock" = {
             "interval" = 60;
-            "format" =  "{:%a, %d %b %H:%M}";
+            "format" = "{:%a, %d %b %H:%M}";
             "max-length" = 25;
             "rotate" = 0;
           };
           "idle_inhibitor" = {
             "format" = "{icon}";
             "format-icons" = {
-              "activated" = ''<span size="x-large"> 󰛊 </span>'';
-              "deactivated" = ''<span size="x-large"> 󰾫 </span>'';
+              "activated" = ''<span size="x-large">󰛊 </span>'';
+              "deactivated" = ''<span size="x-large">󰾫 </span>'';
             };
             "timeout" = 60;
           };
           "tray" = { "spacing" = 6; };
+          "wireplumber" = {
+            "format" = "";
+            "format-muted" = ''<span size="x-large">󰖁</span>'';
+          };
+          "wlr/taskbar" = {
+            "icon-theme" = [ "Yaru" "Hicolor" ];
+            "icon-size" = 20;
+            "tooltip-format" = "{title}";
+            "on-click" = "activate";
+            "on-click-middle" = "close";
+          };
         };
       };
       style = ''
@@ -64,6 +78,9 @@
         }
         #clock {
           padding-right: 6px;
+        }
+        #taskbar button.active {
+          background-color: rgba(214, 199, 199, 1);
         }
       '';
     };
