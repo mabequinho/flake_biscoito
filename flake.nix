b34eca7ae52b3a8a7fd84d52bd47217c5326685a
Lucio <46428150+mabequinho@users.noreply.github.com>
2023-07-13 13:25:44 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Biscoito";
 
   inputs = {
-
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Home manager
@@ -19,36 +18,25 @@
       url = "github:mabequinho/touhou-cursors";
       flake = false;
     };
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, homeage, touhou-cursors, hyprland
-    , hyprland-contrib, hyprland-plugins, ... }: {
-      nixosConfigurations = {
-        biscoito = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./nixos/configuration.nix
-            hyprland.nixosModules.default
-            { programs.hyprland.enable = true; }
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = false;
-                useUserPackages = true;
-                users.mabeco = import ./home/home.nix;
-                extraSpecialArgs = { inherit inputs; };
-              };
-            }
-          ];
-        };
+outputs = { nixpkgs, home-manager, touhou-cursors, homeage ... }@inputs: {
+
+    nixosConfigurations = {
+      biscoito = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [ ./nixos/configuration.nix ];
       };
     };
+
+    homeConfigurations = {
+      "mabeco@Biscoito" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+           modules = [ ./home-manager/home.nix ];
+      };
+    };
+
+  };
 
 }
